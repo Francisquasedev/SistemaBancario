35ef6e7eba4fd5b481bfd6374d4ac96dc2e15b13
Quasedev__ <167344752+Francisquasedev@users.noreply.github.com>
2024-05-16 17:12:13 -0300

    Add files via upload

---
+++ desafio_python/README.MD
@@ -0,0 +1,39 @@
+# Sistema Bancário Versão 1.0
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)
+
+
+## Descrição
+Este é um sistema bancário simples desenvolvido em Python. Ele permite aos usuários realizar operações básicas como depósito, saque e visualização do extrato da conta.
+
+## Linguagem
+Este código foi escrito em Python
+
+## Instruções de Instalação
+1. Certifique-se de ter o Python instalado.
+2. Clone ou baixe este repositório em sua máquina local.
+3. Navegue até o diretório onde o código está localizado.
+4. Execute o código em um ambiente Python.
+
+## Pré-requisitos
+Pra usar este sistema, você precisa ter o Python instalado em seu sistema.
+
+## Intrções de Uso
+1. Execute o código em um ambiente Python.
+2. Escolha uma das oções do menu:
+    - [1] Depósito: Realize depósitos na conta.
+    - [2] Saque: Realize saques na conta (com limite máximo de R$500 por saque e máximo de 3 saques).
+    - [3] Extrato: Visualize as movimentações da conta e o saldo atual.
+    - [0] Sair: Encerre o programa.
+3. Siga as instruções na tela para realizar as operações desejadas.
+
+## Licença
+Este projeto é licenciado sob a [![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)
+
+## Contribuidores
+- Francis Monteles
+
+## Contato
+Se você tiver alguma dúvida, crítica ou sugestão pode entrar em contato comigo. Estou disponível via e-mail ou através das redes sociais.
+
+- **E-mail:**
+- **LinkedIn:** [Francis Monteles]()# BankSystem
---
+++ desafio_python/sistema_bancario.py
@@ -0,0 +1,68 @@
+# Sitema Bancário Versão 1.0
+
+# Inicializando variáveis de saldo e contadores de depósitos e saques
+saldo = 0
+qtd_dep = 0
+qtd_saque = 0
+
+while True:
+    # Exibindo o menu inicial e solicitando uma escolha  do usuário
+    menu_inicial = int(input('''Escolha uma das opções:
+    [1] Depósito
+    [2] Saque
+    [3] Extrato
+    [0] Sair
+    ''')) 
+
+    # ÍNICIO DEPÓSITO
+    if menu_inicial == 1:
+        print("Você escolheu a opção Depósito.")
+        deposito = float(input("Informe o valor que será depositado: "))
+        if deposito > 0:
+            # Realizando o depósito se o valor for positivo
+            print("Depósito realizado com sucesso!")
+            saldo += deposito
+            print(f"Valor depositado: R$ {deposito:.2f}")
+            print(f"Saldo: R$ {saldo:.2f}")
+            qtd_dep += 1
+        else:
+            print("Valor informado é inválido.")
+    # FIM DEPÓSITO
+            
+    # ÍNICIO SAQUE
+    elif menu_inicial == 2:
+        print("Você escolheu a opção Saque.")
+        while qtd_saque < 3:
+            saque = float(input("Informe o valor que gostaria de sacar: "))
+            if saque > 500:
+                print("Valor máximo permitido é de 500.")
+                continue
+            if saque > saldo:
+                print("Saldo insuficiente.")
+                continue
+            # Realizando o saque se o valor estiver dentro dos limites
+            saldo -= saque
+            print("Saque realizado com sucesso!")
+            print(f"Valor do saque: R$ {saque:.2f}")
+            print(f"Saldo: R$ {saldo:.2f}")
+            qtd_saque += 1
+    # FIM SAQUE
+
+    # ÍNICIO EXTRATO
+    elif menu_inicial == 3:
+        print("Você escolheu a opção Extrato.")
+        print("Visualizar Extrato")
+        if qtd_saque != 0 and qtd_dep != 0:
+            # Exibindo informações do extrato se houver movimentações na conta
+            print(f"Saques realizados: {qtd_saque}")
+            print(f"Depósitos realizados: {qtd_dep}")
+            print(f"Saldo: R$ {saldo:.2f}")
+        else:
+            print("Não foram realizadas movimentações na conta.")
+    # FIM EXTRATO
+
+    # SAIR 
+    elif menu_inicial == 0:
+        print("Você escolheu a opção Sair.")
+        print("Obrigado por usar este sistema. Até mais!!!")
+        break
